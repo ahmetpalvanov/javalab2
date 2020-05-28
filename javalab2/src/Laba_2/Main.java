b3a9d24073971909461f74bb3ffef928942c52e7
palvanovamet <66084096+palvanovamet@users.noreply.github.com>
2020-05-28 21:27:45 +0300

    Add files via upload

---
+++ javalab2/src/Laba_2/Main.java
@@ -0,0 +1,14 @@
+package Laba_2;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		formula frame = new formula();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
